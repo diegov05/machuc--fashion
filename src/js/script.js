04af656c24ab07be8da336726dd48071bc56dcc5
diegov05 <diegovs0504@gmail.com>
2022-09-29 12:47:18 -0400

    LANGUAGE SELECTOR JQUERY

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+$(function () {
+  $("#language-selector-container").hover(
+    function () {
+      $(".language-selector-text").css("color", "black");
+    },
+    function () {
+      $(".language-selector-text").css("color", "white");
+    }
+  );
+});
+
+$(function () {
+  $(".li4-btn--subtle").hover(
+    function () {
+      $(".li4-icon").css("color", "black");
+    },
+    function () {
+      $(".li4-icon").css("color", "white");
+    }
+  );
+});
